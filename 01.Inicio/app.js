56235bae5a9180e793184d016c3db2d142e0d596
Edin <edinvasquez412@gmail.com>
2020-09-04 22:56:54 -0500

    Agregue dos carpetas: Notas de JavaScript,.gitignore; para tener un orden. Hasta aqui me encuentro en lo basico de javascript

--- 01.Inicio/app.js
+++ 01.Inicio/app.js
@@ -1,1 +1,7 @@
-/*  */
+let nombre = prompt ('"¿Cual es tu nombre?"');
+let edad = prompt ('¿Cual es tu edad?');
+let pais = prompt ('¿De donde eres?');
+
+document.getElementById('app').innerHTML = `Bienvenido ${nombre} de ${edad} años.<br>` + `Eres de ${pais}.`;
+/* Utilizar <br> para salto de linea en JavaScript */
+
