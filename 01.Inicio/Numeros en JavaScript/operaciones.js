e0d5ad0ea3087aa1a573188ffb43bcd2a86374bb
Edin <edinvasquez412@gmail.com>
2020-09-07 20:39:26 -0500

    Aqui estoy en la parte de numeros y operaciones de JavaScript

---
+++ 01.Inicio/Numeros en JavaScript/operaciones.js
@@ -0,0 +1,79 @@
+// ###############   OPERACIONES EN JAVSCRIPT ##########
+
+    //En JavaScript cuando queremos hacer una operacion, tenemos que tener cuidado, existen la regla de poner "Number" antes de cada variable asignada. Para que esta la interprete como un numero y no como una cadena de texto, asi no termine uniendo los numeros sino devolviendo el resultado de acuerdo a la operacion.
+
+// SUMA
+/* var primerNumero = prompt('Ingresa primer numero'),
+    segundoNumero = prompt('Ingresa segundo numero'),
+    sumaTotal = Number(primerNumero) + Number(segundoNumero); */
+
+/* document.write(sumaTotal); */
+
+// Vamos hacer que muestre los nu7mero sumados y el resultado
+/* document.write(primerNumero+'<br>');
+document.write(segundoNumero+'<br>'+'___'+'<br>');
+document.write(sumaTotal); */
+
+// Otro ejemplo:
+
+const numero1 = 10,
+    numero2 = -22,
+    numero3 = 30,
+    numero4 = 40,
+    numero5 = 50;
+
+let resultado;
+// Adición
+resultado = numero1 + numero2;
+// Resta
+resultado = numero4 - numero3;
+// Multiplicacion
+resultado = numero5 * numero4;
+// División
+resultado = numero3 / numero5;
+// Porcentaje / Modulo
+resultado = numero5 % numero1;
+// Valor de pi | PI
+resultado = Math.PI;
+// Redondeo
+resultado = Math.round(2.6);
+// En Redondeo ahi dos funciones mas si queremos:
+    //El ".ceil" nos permite redondear todo decimal al numero entero siguiente.
+    resultado = Math.ceil(2.25);
+    //El ".floor" nos permite redondear todo decimal al numero entero en el que se encuentra.
+    resultado = Math.floor(2.99);
+// Raiz cuadrada
+resultado = Math.sqrt(144);
+// Absoluto | Devuelve numeros negativos a positivos
+resultado = Math.abs(numero2);
+// Potencia
+resultado = Math.pow(8,3);
+// Minimo | Devuelve el numero minimo que tengamos.
+resultado = Math.min(52,8,10,99,63,84);
+// Maximo | Devuelve el numero maximo que tengamos.
+resultado = Math.max(52,8,10,99,63,84);
+// Aleatorio
+resultado = Math.random(1,10);
+
+resultado = 100 * 0.2;
+
+//_________________
+let puntaje = 10,
+    puntaje1 = 20,
+    puntaje2 = 30,
+    puntaje3 = 40;
+    puntaje4 = 100;
+    puntaje5 = 150;
+    puntaje6 = 250;
+    puntaje7 = 400;
+
+
+console.log(puntaje++); // Esto incrementa en el orden de numeros, despues de lo que imprima el especificado.
+console.log(puntaje1--); // Esto decrementa en el orden de numeros, despues de lo que imprima el especificado.
+console.log(++puntaje2); // Esto incrementa en el orden de numeros desde ya.
+console.log(--puntaje3); // Esto decrementa en el orden de numeros desde ya.
+
+console.log(puntaje4 += 35); // Esto suma el dato dado con lo establecido.
+console.log(puntaje5 -= 42); // Esto suma el dato dado con lo establecido.
+
+
--- 01.Inicio/app.js
+++ 01.Inicio/app.js
@@ -43,3 +43,70 @@ console.log(frase1);
 
 mensaje = 'Java' + 'Script';
 console.log(mensaje);
+
+//UNIENDO VARIABLES EN UNA SOLA
+
+let aprendiendo = 'Aprendiendo',
+    tecnologia = 'JavaScript';
+
+console.log(aprendiendo+' '+tecnologia);
+
+// TEMPLATES LITERALS
+
+let learning = 'Aprendiendo',
+    technology = 'Python';
+
+console.log(`${learning} ${technology}`)
+
+
+// ###### METODOS QUE SON APLICABLES SOLO A CADENAS DE TEXTOS ######
+
+// METODO length (.length) | Para saber cuantas letras tiene una palabra.
+
+let palabra = 'JavaScript';
+console.log(palabra.length);
+
+// METODO concat (.concat) | Para unir con una cadena predefinida.
+
+let palabra1 = 'Java';
+console.log(palabra.concat(' ' + 'es un lenguaje popular'));
+
+// METODO toUpperCase (.toUpperCase) | Para convertir cadenas minusculas a mayusculas.
+
+let palabra2 = 'C es un gran lenguaje de programacion';
+console.log(palabra2.toUpperCase());
+
+// METODO toLowerCase (.toLowerCase) | Para convertir cadenas minusculas a mayusculas.
+
+let palabra3= 'C# ES MANTENIDO POR Microsoft';
+console.log(palabra3.toLowerCase());
+
+// METODO indexOf (.indexOf) | Para buscar palabras de acuerdo a lo especificado. Este devuelve en numero donde encuentra lo indicado, de lo contrario muestra -1, porque no encuentra lo especificado.
+
+let palabra4= 'Aprederemos HTML5, CSS3 y JavaScript para ser un desarrollador FrontEnd';
+console.log(palabra4.indexOf('FrontEnd'));
+
+// METODO substring (.substring) | Para indicar de donde a donde muestre el string de acuerdo a lo especificado entre parentesis inicio y fin.
+
+let palabra5= 'Aprederemos HTML5, CSS3 y JavaScript para ser un desarrollador FrontEnd';
+console.log(palabra5.substring(03,15));
+
+// METODO split (.split) | Es para ver cuantos espacios tiene nuestro texto asi sabremos cuantas palabras existen en nuestra cadena de texto. Tomara de acuerdo a lo que le indiquemos.
+
+let palabra6= 'Aprederemos HTML5, CSS3 y JavaScript para ser un desarrollador FrontEnd';
+console.log(palabra6.split(' '));
+
+// METODO replace (.replace) | Es para cambiar palabras con lo especificado.
+
+let palabra7= 'Aprederemos HTML5, CSS3 y JavaScript para ser un desarrollador FrontEnd';
+console.log(palabra7.replace('CSS3','CSS'));
+
+// METODO includes (.includes) | Es para verificar que si la palabra existe de acuerdo a lo especificado. Si existe la palbra devolvera "true", o de lo contrario sera "false".
+
+let palabra8= 'Aprederemos HTML5, CSS3 y JavaScript para ser un desarrollador FrontEnd';
+console.log(palabra8.includes('FrontEnd'));
+
+// METODO repeat (.repeat) | Esto repetira nuestra cadena de texto las veces que la especifiquemos.
+
+let palabra9= 'Esto se repetira las veces indicadas'+' ';
+console.log(palabra9.repeat(10));
